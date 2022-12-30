6971c39719a77b4fd50cbce4c5b6682d82fc96de
seba vargas <sebavargasd2014@gmail.com>
2022-12-30 02:30:19 -0300

    aa

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-'use strict';
+"use strict";
 
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
 const snap = document.getElementById("snap");
-const errorMsgElement = document.querySelector('span#errorMsg');
+const errorMsgElement = document.querySelector("span#errorMsg");
 
+const aspectRatioo = settings.width / settings.height;
 
 const constraints = {
   audio: false,
-//   video: {
-//     width: {
-//       min: 390,
-//       ideal: 1920,
-//       max: 2560,
-//     },
-//     height: {
-//       min: 720,
-//       ideal: 1080,
-//       max: 1440
-//     },
-//     facingMode: { exact: 'environment' }
-//   }
-    video: { facingMode: { exact: "environment" },
-    width: screen.width, height: screen.height
-
- }
-
-
+  //   video: {
+  //     width: {
+  //       min: 390,
+  //       ideal: 1920,
+  //       max: 2560,
+  //     },
+  //     height: {
+  //       min: 720,
+  //       ideal: 1080,
+  //       max: 1440
+  //     },
+  //     facingMode: { exact: 'environment' }
+  //   }
+  video: {
+    facingMode: { exact: "environment" },
+    width: screen.width,
+    height: screen.height,
+    frameRate: { max: 30 },
+    aspectRatio: 0.4634146341463415
+  },
 };
 
 // Access webcam
@@ -49,7 +51,7 @@ function handleSuccess(stream) {
 init();
 
 // Draw image
-var context = canvas.getContext('2d');
-snap.addEventListener("click", function() {
-        context.drawImage(video, 0, 0, 640, 480);
+var context = canvas.getContext("2d");
+snap.addEventListener("click", function () {
+  context.drawImage(video, 0, 0, 640, 480);
 });
